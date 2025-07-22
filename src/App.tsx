7a9ce3a7b4445f595610088aa2dfc8b638b3abe0
João Pedro Pizoli Carvalho <joao.carvalho@mococa.com>
2025-07-22 11:42:28 -0300

    Cria o Countdown e o DefaultInput

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Logo } from './components/Logo'
 import { Container }  from './components/Container'
 import { Menu } from './components/Menu'
+import { CountDown } from './components/CountDown'
 
 import './styles/global.css'
 import './styles/theme.css'
+import { DefaultInput } from './components/DefaultInput'
 
 export function App(){
   return (
@@ -15,7 +17,32 @@ export function App(){
     <Container>
       <Menu/>
     </Container>
-      
+
+    <Container>
+      <CountDown/>
+    </Container>
+
+    <Container>
+      <form action="" className='form'>
+        <div className='formRow'>
+          <DefaultInput  labelText='salve' id='input' type='text' />
+        </div>
+
+        <div className='formRow'>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+
+        <div className='formRow'>
+          <p>Ciclos</p>
+          <p>0 0 0 0 0 0</p>
+        </div>
+
+        <div className='formRow'>
+          <button>Enviar</button>
+        </div>
+      </form>
+    </Container>
+
     </>
   )
 }
